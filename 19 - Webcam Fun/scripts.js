beeae136fcf7014ce2df8b1909264371bc1707dd
Ashrf Ali <ashrftvm1@gmail.com>
2019-09-03 17:00:10 +0400

    added script to rgbsplit and red effect

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -26,7 +26,29 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; //Red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //Green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //Blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 150] = pixels.data[i + 0]; //Red
+    pixels.data[i + 100] = pixels.data[i + 1]; //Green
+    pixels.data[i - 150] = pixels.data[i + 2]; //Blue
+  }
+  return pixels;
 }
 
 function takePhoto() {
